aacafda1b3dfc2d4b4aa5a70b18fda199d2ed06b
Jawwad <fahadmallick99@gmail.com>
2023-12-31 03:59:48 +0530

    added contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Announcements from './components/Announcements';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <Announcements/>
     <About/>
+    <Contact/>
 
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,36 @@
-// import React from 'react'
-
-// const Navbar = () => {
-//   // const navbarHeight = 24; 
-//   return (
-//     <nav className='flex justify-between items-center mx-auto px-4 h-24 max-w-[1240px] text-[#DDDDDD]'>
-//       <h1 className='w-full text-3xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] '>St Mary High School</h1>
-//         <ul className='list-none flex text-xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
-//             <li className='p-4 btn'>Home</li>
-//             <li className='p-4 btn'>About</li>
-//             <li className='p-4 btn'>Announcements</li>
-//             <li className='p-4 btn'>Admission</li>
-//             <li className='p-4 btn'>Contact</li>
-//         </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
-
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+
+  const [dropdown, setdropdown] = useState(false);
+
+  const handleMouseOver = ()=> {
+    setdropdown(!dropdown);
+  }
+
   return (
-    <nav className='flex justify-between items-center mx-auto px-4 h-24 max-w-[1240px] text-[#30475E]'>
+    <nav className='flex justify-between items-center mx-auto px-4 h-24 text-[#30475E] bg-white w-full sticky top-0 z-50'>
       <h1 className='w-full text-3xl font-bold  '>St Mary High School</h1>
         <ul className='list-none flex text-xl'>
             <li className='p-4 btn'>Home</li>
             <li className='p-4 btn'>About</li>
-            <li className='p-4 btn'>Announcements</li>
-            <li className='p-4 btn'>Admission</li>
             <li className='p-4 btn'>Contact</li>
+            <li className='p-4 btn' onMouseEnter = {handleMouseOver} onMouseLeave = {handleMouseOver}>More
+            
+            {dropdown && (
+               <ul className='relative top-12 bg-white border border-gray-200 '>
+               <li className='p-2'>Admission</li>
+               <li className='p-2'>Gallery</li>
+               <li className='p-2'>Alumni</li>
+               <li className='p-2'>Calendar</li>
+             </ul>
+            )}
+            </li>
+            <li className='p-4 btn'>Login</li>
         </ul>
     </nav>
   )
 }
 
 export default Navbar
+
