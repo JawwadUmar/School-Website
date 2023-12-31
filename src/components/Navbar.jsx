486d536002d8c5ef62d0f9da23380e3002c9f649
Jawwad <fahadmallick99@gmail.com>
2023-12-31 05:50:39 +0530

    set the side nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,12 +63,12 @@ const Navbar = ({isOpen, setisOpen}) => {
       <ul
         className={
           isOpen
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#DDDDDD] ease-in-out duration-500"
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
-          Educators Online
+        <h1 className="w-full text-3xl font-bold text-[#30475E] m-4">
+          St Mary High School
         </h1>
         <li className="p-4 border-b border-gray-600">Home</li>
         <li className="p-4 border-b border-gray-600">About</li>
